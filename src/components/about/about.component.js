65ac93462207d027250d2e6d7b5f975f07a09b95
Elliot-Evans-95 <ell15evans.nuls@googlemail.com>
2017-04-14 12:23:47 +0100

    added a loop on frameworks so that framework now pull from the about json file. Added a hover effect on the social media links

--- src/components/about/about.component.js
+++ src/components/about/about.component.js
@@ -20,19 +20,22 @@ app.component('aboutPage', {
                 console.log('response', response);
 
                 for (var responseImages = 0; responseImages < response.data.language.length; responseImages++) {
-                    console.log('BEFORE', response.data.language[responseImages].image);
                     response.data.language[responseImages].image = require(`${response.data.language[responseImages].image}`);
-                    console.log('AFTER', response.data.language[responseImages].image);
-                    console.log('Length: ', responseImages);
+                }
+
+                for (var responseImages = 0; responseImages < response.data.framework.length; responseImages++) {
+                    response.data.framework[responseImages].image = require(`${response.data.framework[responseImages].image}`);
                 }
 
                 let _pageHeading = response.data.pageHeading;
                 let _pageSubHeading = response.data.pageSubHeading;
                 let _languages = response.data.language;
+                let _frameworks = response.data.framework;
 
-                $scope.languages = _languages;
                 $scope.pageHeading = _pageHeading;
                 $scope.pageSubHeading = _pageSubHeading;  
+                $scope.languages = _languages;
+                $scope.frameworks = _frameworks;
 
             }, (response, status) => {
 

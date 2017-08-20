56deecdb93ee5a68a94c8537466efe825a292ad7
Elliot Evans <ell15evans.nuls@googlemail.com>
2017-08-20 14:31:51 +0100

    created a button component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './button';
 
 class App extends Component {
   render() {
@@ -11,8 +12,10 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload, cool!.
+          To get started, edit <code>src/App.js</code> and save to reload,
+          cool!.
         </p>
+        <Button message="Click Me!" className="App-button" />
       </div>
     );
   }

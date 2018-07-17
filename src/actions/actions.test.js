9df30a1ea723f2664d64e0e464e533921940f5fc
Elliot Evans <ell15evans.nuls@googlemail.com>
2018-07-18 00:09:13 +0100

    added a redux action test

---
+++ src/actions/actions.test.js
@@ -0,0 +1,14 @@
+import * as actions from './index';
+import * as types from './../constants/action-types';
+
+describe('actions', () => {
+  it('should create an action to add an article', () => {
+    const payload = 'Finish docs';
+    const expectedAction = {
+      type: types.ADD_ARTICLE,
+      payload
+    };
+
+    expect(actions.addArticle(payload)).toEqual(expectedAction);
+  });
+});
--- src/route.test.js
+++ src/route.test.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import { mount } from 'enzyme';
-
 import { MemoryRouter } from 'react-router';
-import { Provider } from 'react-redux';
 
 import Home from './containers/Pages/HomePage/home';
-import About from './containers/Pages/AboutPage/about';
-import Work from './containers/Pages/WorkPages/work';
-import Contact from './containers/Pages/ContactPage/contact';
-import store from './store';
 import Root from './route';
 
 const renderRoutes = path =>
@@ -24,22 +18,4 @@ describe('#routes', () => {
 
     expect(component.find(Home)).toHaveLength(1);
   });
-  //
-  // test("renders about page for about", () => {
-  //   const component = renderRoutes("/about");
-  //
-  //   expect(component.find(About)).toHaveLength(1);
-  // });
-  //
-  // test("renders work page for /work url", () => {
-  //   const component = renderRoutes("/courses");
-  //
-  //   expect(component.find(Work)).toHaveLength(1);
-  // });
-  //
-  // test("renders contact manage page for /course", () => {
-  //   const component = renderRoutes("/contact");
-  //
-  //   expect(component.find(Contact)).toHaveLength(1);
-  // });
 });

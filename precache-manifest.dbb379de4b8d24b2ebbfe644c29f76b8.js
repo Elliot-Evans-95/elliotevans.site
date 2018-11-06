5ddcf9e4ab389d0e3cb0a6caa8547841118ede97
Elliot Evans <ell15evans.nuls@googlemail.com>
2018-11-06 01:52:30 +0000

    Updates

---
+++ precache-manifest.dbb379de4b8d24b2ebbfe644c29f76b8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7504847ec68d12d133a",
+    "url": "/static/css/main.602334b5.chunk.css"
+  },
+  {
+    "revision": "c7504847ec68d12d133a",
+    "url": "/static/js/main.c7504847.chunk.js"
+  },
+  {
+    "revision": "c093666f28f0d6762056",
+    "url": "/static/js/1.c093666f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "afa2252f281ad00eb15f2a721b2c1471",
+    "url": "/static/media/06-08-2018-hello-world.afa2252f.md"
+  },
+  {
+    "revision": "5750563ba578aef28443b4a53e572a51",
+    "url": "/static/media/07-08-2018-another-one.5750563b.md"
+  },
+  {
+    "revision": "2c25baf0774bfbf4a68bf359c0369c68",
+    "url": "/static/media/home.2c25baf0.svg"
+  },
+  {
+    "revision": "9d191e384c5ab7460afc4ca0032b4af1",
+    "url": "/static/media/about.9d191e38.svg"
+  },
+  {
+    "revision": "6740d995d489330f93e8a2c2e37814e4",
+    "url": "/static/media/work.6740d995.svg"
+  },
+  {
+    "revision": "6ae525c90b6d9fe1ccf7c7d3ea0583ec",
+    "url": "/static/media/post.6ae525c9.svg"
+  },
+  {
+    "revision": "6f90d42cfa2ca5623be6188e0c3cbc1e",
+    "url": "/static/media/contact.6f90d42c.svg"
+  },
+  {
+    "revision": "50de90a0feaf2463e36063b6a59e722d",
+    "url": "/index.html"
+  }
+];
---
+++ static/media/06-08-2018-hello-world.afa2252f.md
@@ -0,0 +1,8 @@
+---
+path: "/post/06-08-2018-hello-world"
+date: "2017-11-07"
+title: "My first blog post"
+introduction: "My first blog post is Hello World"
+---
+
+# Hello World

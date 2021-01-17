e84cb2d9e006495e24400fd9ff6b2633135406c4
Elliot Evans <ell15evans.nuls@googlemail.com>
2021-01-17 00:25:17 +0000

    added head component

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -13,21 +13,21 @@ const head = ({ title, description, keywords }: HeadProps): ReactElement => {
     <Helmet>
       {/* <!-- Primary Meta Tags --> */}
       <title>{title}</title>
-      <meta name='title' content={title} />
-      <meta name='description' content={description} />
-      <meta name='keywords' content={keywords} />
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
       {/* <!-- Open Graph / Facebook --> */}
-      <meta property='og:type' content='website' />
-      <meta property='og:url' content='http://myawesomewebsite.com/' />
-      <meta property='og:title' content={title} />
-      <meta property='og:description' content={description} />
-      <meta property='og:image' content='/facebookimage.png' />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="http://myawesomewebsite.com/" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="/facebookimage.png" />
       {/* <!-- Twitter --> */}
-      <meta property='twitter:card' content='summary_large_image' />
-      <meta property='twitter:url' content='http://myawesomewebsite.com/' />
-      <meta property='twitter:title' content={title} />
-      <meta property='twitter:description' content={description} />
-      <meta property='twitter:image' content='/twitterimage.png' />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="http://myawesomewebsite.com/" />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content="/twitterimage.png" />
     </Helmet>
   );
 };
